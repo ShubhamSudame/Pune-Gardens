5b9872b55c605ee7d2bd5fa4eb4ce35ad442e139
Shubham Sudame <shubham.sudame@gmail.com>
2017-01-09 08:37:07 +0530

    Added Markers of Gardens

--- Webmap.py
+++ Webmap.py
@@ -4,11 +4,15 @@ import pandas
 df = pandas.read_csv("dataset.csv")
 LAT = df['LAT'].mean() #Creating our own macros in Python.
 LON = df['LON'].mean()
-DEFAULT = 6
+DEFAULT = 15
 
 map = folium.Map(location = [LAT,LON],zoom_start = DEFAULT)
+fg = folium.FeatureGroup(name = 'Garden Locations') #Making a feature group so that we can select all gardens at once.
 
-map.simple_marker(location =[18.5467,73.7925],popup='Home',marker_color='green')
-map.simple_marker(location =[18.5312,73.8557],popup='College',marker_color='red')
-#Hello.
+for lat,lon,name in zip(df['LAT'],df['LON'],df['NAME']):        # To create Garden markers by traversing through the file.
+    fg.add_child(folium.Marker(location =[lat,lon],popup=name,icon=folium.Icon(color='green')))
+map.add_child(fg)
+
+map.add_child(folium.LayerControl())        #Creates filters on drop down menu on map of all the things under add_child.
+
 map.save('map.html')
